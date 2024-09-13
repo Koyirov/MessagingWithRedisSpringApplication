6fd77d77d66b9f4663fbc8b85b922845b937df0e
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2024-09-13 14:28:57 +0200

    initial message

---
+++ src/test/java/com/example/messagingwithredisspring/MessagingWithRedisSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messagingwithredisspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingWithRedisSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
